5845aa082e5be9b217a78e7cd2e1b423bc7e3e70
Haytham Akl <silents.coders@gmail.com>
2017-02-23 21:45:28 -0500

    Slight modifications
    Adding recipes dummy data

--- components/App.js
+++ components/App.js
@@ -1,9 +1,47 @@
 import React, {Component } from 'react'
-import Posts from './Posts'
+import Recipes from './Recipes'
 
 
 class App extends Component {
 
+constructor(){
+    this.state = {
+        recipes: [
+            {
+                _Id : '3423342',
+                User:'123233dds2',
+                Title:'Recipe #1',
+                Ingredients : 'Ingredients #1',
+                Directions  : 'Directions #1'
+            },
+            {
+                _Id : '3422342',
+                User:'111233dds2',
+                Title:'Recipe #2',
+                Ingredients : 'Ingredients #2',
+                Directions  : 'Directions #2'
+            },{
+                _Id : '34223242',
+                User:'120033dds2',
+                Title:'Recipe #3',
+                Ingredients : 'Ingredients #3',
+                Directions  : 'Directions #3'
+            },{
+                _Id : '34444342',
+                User:'100dds2',
+                Title:'Recipe #4',
+                Ingredients : 'Ingredients #4',
+                Directions  : 'Directions #4'
+            },{
+                _Id : '3421142',
+                User:'123533ds2',
+                Title:'Recipe #5',
+                Ingredients : 'Ingredients #5',
+                Directions  : 'Directions #5'
+            }
+        ]
+    }
+}
 render(){
     
     return (
---
+++ components/Recipes.js
@@ -0,0 +1,22 @@
+import React, {Component } from 'react'
+
+
+
+class Recipes extends Component {
+
+render(){
+    
+    return (
+        
+        <div className="Recipes">
+        
+Recipes
+
+        </div> // End of Recipes </div>   
+    )       
+}
+}
+
+
+
+export default Recipes
